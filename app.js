1e919896911f75f618ebe515d9c143b0e154a771
Jingyi Wang <shizuwang1209@gmail.com>
2017-12-19 18:05:27 -0800

    feat:Add CORS

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const routes = require("./server/routes/routes");
 
-// Set up the express app
-const app = express();
-
-// Log requests to the console.
 app.use(logger("dev"));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const originWhiteList = ["http://localhost:3000", "http://45.55.12.226"];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(originWhiteList.indexOf(origin) === -1){
+      let msg = 'Not allowed origin';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 routes(app);
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,27 @@
-const signupController = require('../controllers/authenticate').signup;
-const loginController = require('../controllers/authenticate').login;
-const verifyToken = require('../middleware/verifyToken');
-module.exports = (app)=>{
-    app.post('/api/signup', signupController);
-    app.post('/api/login', loginController);
-    app.get('/api/test', verifyToken, (req, res, next)=>{
-        res.send('This is a test for jwt!');
+const cors = require("cors");
+
+const signupController = require("../controllers/authenticate").signup;
+const loginController = require("../controllers/authenticate").login;
+const verifyToken = require("../middleware/verifyToken");
+
+
+
+    const whitelist = ["http://localhost:3000", "http://45.55.12.226"];
+    const corsOptionsDelegate = function(req, callback) {
+    let corsOptions;
+    if (whitelist.indexOf(req.header("Origin")) !== -1) {
+        corsOptions = { origin: true };
+    } else {
+        corsOptions = { origin: false };
+    }
+    callback(null, corsOptions);
+};
+
+module.exports = app => {
+
+    app.post("/api/signup", signupController);
+    app.post("/api/login", loginController);
+    app.get("/api/test", verifyToken, (req, res, next) => {
+        res.send("This is a test for jwt!");
     });
 };
